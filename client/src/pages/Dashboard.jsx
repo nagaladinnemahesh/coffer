2172b9cfc66c93e9c1d04f2166f2cb191c862833
Mahesh Nagaladinne <maheshnagaladinne@gmail.com>
2025-12-24 13:09:40 +0530

    fix: vercel dashboard deployment

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import "../styles/dashboard.css";
+import "../styles/Dashboard.css";
 import api from "../axios";
 import { showSuccess, showError } from "../utils/toast";
 import ComposeModal from "../components/ComposeModal.jsx";
